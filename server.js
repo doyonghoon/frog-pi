254dc3b0eafb6e23bb7ea1e988b18b6a006836a2
Doyonghoon <magicdo7@gmail.com>
2013-12-07 01:42:31 +0900

    GPIO 17번 테스트 코드.

--- server.js
+++ server.js
@@ -5,21 +5,36 @@ var Gpio = require('onoff').Gpio, // Constructor function for Gpio objects.
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.write("Hello World");
+	res.write("frog-pi connected...");
 	res.end();
 
-	console.log("frogfrog!! console test");
+	console.log("frog-pi-console connected");
 
-	iv = setInterval(function() {
-	    led.writeSync(led.readSync() === 0 ? 1 : 0); // 1 = on, 0 = off :)
-	console.log("frog: control LED...");
-	}, 200);
+	// iv = setInterval(function() {
+	//     led.writeSync(led.readSync() === 0 ? 1 : 0); // 1 = on, 0 = off :)
+	// }, 200);
 
-	// Stop blinking the LED and turn it off after 5 seconds.
-	setTimeout(function() {
-	    clearInterval(iv); // Stop blinking
-	    led.writeSync(0);  // Turn LED off.
-	    led.unexport();    // Unexport GPIO and free resources
-	}, 5000);
+	// // Stop blinking the LED and turn it off after 5 seconds.
+	// setTimeout(function() {
+	//     clearInterval(iv); // Stop blinking
+	//     led.writeSync(0);  // Turn LED off.
+	//     led.unexport();    // Unexport GPIO and free resources
+	// }, 5000);
+
+	(function blink(count) {
+	    if (count <= 0) return led.unexport();
+
+	    led.read(function(err, value) {  // Asynchronous read.
+	        if (err) throw err;
+
+	        led.write(value === 0 ? 1 : 0, function(err) { // Asynchronous write.
+	            if (err) throw err;
+	        });
+	    });
+
+	    setTimeout(function() {
+	        blink(count - 1);
+	    }, 200);
+	})(20);
 
 }).listen(8080);
